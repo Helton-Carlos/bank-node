62fa3362d7db4de32ed7c6d3173244e40b485890
Helton Brito <heltoncarlospa@hotmail.com>
2023-02-22 10:53:00 -0300

    feat: add function exit session

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function operation() {
       },
     ])
     .then((retorno) => {
-      const action = retorno["action"];
+      let action = retorno["action"];
       action === "Criar Conta" ? message() : '';
+      action === "Sair" ? exitSession() : '';
     })
     .catch((err) => {
       console.log(`Erro ${err}`);
@@ -34,28 +35,39 @@ function message() {
   console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco"));
   console.log(chalk.green("Defina as opções de conta a seguir"));
 
-  buildAccount() 
+  buildAccount();
+}
+
+function  exitSession(){
+  console.log(chalk.bgBlue.black("Obrigado por usar nosso banco!"));
+  process.exit();
 }
 
 function buildAccount() {
   inquirer
-  .prompt([
-    {
-      name: "nameAction",
-      message: "Digite o nome da sua conta:",
-    },
-  ])
-  .then((retorno) => {
-    const answer = retorno["nameAction"];
-    if(!fs.existsSync('accounts')) fs.mkdirSync('accounts');
-    if(fs.existsSync(`accounts/${answer}.json`)){
-      console.log(chalk.bgRed.black("Essa conta já existe")) 
-      buildAccount() ;
-    };
+    .prompt([
+      {
+        name: "nameAction",
+        message: "Digite o nome da sua conta:",
+      },
+    ])
+    .then((retorno) => {
+      const answer = retorno["nameAction"];
+      if (!fs.existsSync("accounts")) fs.mkdirSync("accounts");
+      if (fs.existsSync(`accounts/${answer}.json`)) {
+        console.log(chalk.bgRed.black("Essa conta já existe"));
+        buildAccount();
+      }
 
-    fs.writeFileSync(`accounts/${answer}.json`,'{ "balance" : 0}', function error(err){console.log(`Erro ${err}`);});
-  })
-  .catch((err) => {
-    console.log(`Erro catch ${err}`);
-  });
+      fs.writeFileSync(
+        `accounts/${answer}.json`,
+        '{ "balance" : 0}',
+        function error(err) {
+          console.log(`Erro ${err}`);
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(`Erro catch ${err}`);
+    });
 }
