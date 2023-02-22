af962f09e77a005633629dfd43dc17cef315d11f
Helton Brito <heltoncarlospa@hotmail.com>
2023-02-22 10:40:58 -0300

    feat: add build account

--- index.js
+++ index.js
@@ -33,4 +33,29 @@ function operation() {
 function message() {
   console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco"));
   console.log(chalk.green("Defina as opções de conta a seguir"));
+
+  buildAccount() 
+}
+
+function buildAccount() {
+  inquirer
+  .prompt([
+    {
+      name: "nameAction",
+      message: "Digite o nome da sua conta:",
+    },
+  ])
+  .then((retorno) => {
+    const answer = retorno["nameAction"];
+    if(!fs.existsSync('accounts')) fs.mkdirSync('accounts');
+    if(fs.existsSync(`accounts/${answer}.json`)){
+      console.log(chalk.bgRed.black("Essa conta já existe")) 
+      buildAccount() ;
+    };
+
+    fs.writeFileSync(`accounts/${answer}.json`,'{ "balance" : 0}', function error(err){console.log(`Erro ${err}`);});
+  })
+  .catch((err) => {
+    console.log(`Erro catch ${err}`);
+  });
 }
